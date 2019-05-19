99a73cc94bb4624fa1259e429ee50f22e5d6afe9
Merwann Ayed <37482556+secret-merwann@users.noreply.github.com>
2019-05-19 18:17:01 +0200

    First commit

---
+++ TP01 - Palindrome GUI/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TP01___Palindrome_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saisieTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(95, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(501, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenue sur ce programme !";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(625, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pour me faire fonctionner, rien de plus simple ! Il suffit d\'entrer une phrase ou" +
+    " un mot dans la case";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(439, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ci-dessous et je vous indique si oui, ou non il s\'agit d\'un palindrome !";
+            // 
+            // saisieTxt
+            // 
+            this.saisieTxt.Location = new System.Drawing.Point(132, 214);
+            this.saisieTxt.Name = "saisieTxt";
+            this.saisieTxt.Size = new System.Drawing.Size(421, 22);
+            this.saisieTxt.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(167, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(365, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Entrez votre phrase ou votre mot ici :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(292, 69);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Dis-moi s\'il s\'agit d\'un palindrome";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(680, 382);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.saisieTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Est-ce un palindrome ?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox saisieTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TP01 - Palindrome GUI/Form1.cs
@@ -0,0 +1,93 @@
+﻿/* 
+ * 
+ * Auteur : AYED Merwann 
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP01___Palindrome_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+        // Fonction pour enlever les accents
+        static string RemoveDiacritics(string text) {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString) {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark) {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        // Fonction pour renverser la chaine de caractères
+        public static string Renverser(string texte) {
+            // On met le texte à renverser dans un tableau de caractères
+            char[] charArray = texte.ToCharArray();
+            // La fonction Array.Reverse va nous permettre d'inverser tout nos caractères dans le tableau et les replacer dedans
+            Array.Reverse(charArray);
+            // Il n'y a plus qu'à retourner une nouvelle chaîne de caractères issue du tableau (comme une sorte de reconstruction)
+            return new string(charArray);
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+
+                string chaine = saisieTxt.Text;
+
+                // 1 ère étape : j'enlève la ponctuation de la 
+                String noPonctuation = Regex.Replace(chaine, @"\p{P}", "");
+
+                // 2 ème étape : tout mettre en minuscule
+                String noMajuscule = noPonctuation.ToLower();
+
+                // 3 ème étape : remplacer les accents, pour cela j'utilise une fonction prise sur le Web et très connue
+                String noAccents = RemoveDiacritics(noMajuscule);
+
+                // 3ème étape bis : j'enlève les espaces de la chaine
+                String noSpaces = noAccents.Replace(" ", String.Empty);
+
+                // 4 ème étape : renverser la chaîne
+                String chaineRenversee = Renverser(noSpaces);
+
+                // 5 ème étape : Tester s'il s'agit bien d'un palindrome
+                if (chaineRenversee == noSpaces) {
+                    MessageBox.Show("Il s'agit bel et bien d'un palindrome !", "Gagné !");
+                } else {
+                    MessageBox.Show("Ce n'est pas un palindrome !", "Désolé !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+            } catch {
+                MessageBox.Show("Erreur !", "Une erreur a été rencontrée, veuillez réessayer.",
+    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
